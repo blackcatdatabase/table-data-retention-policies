17380cba74a019c8362ee140577118a57ad769e7
Vito416 <vito416@protonmail.com>
2025-12-05 22:04:04 +0100

    Fixed deep problems in schema, added safe checks not to repeat in future, using sha1 now in comment

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -1,6 +1,8 @@
--- Auto-generated from schema-map-postgres.yaml (map@74ce4f4)
+-- Auto-generated from schema-map-postgres.yaml (map@sha1:F0EE237771FBA8DD7C4E886FF276F91A862C3718)
 -- engine: postgres
 -- table:  data_retention_policies
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_drp_entity_scope ON data_retention_policies (entity_table, field_name, action, keep_for);
 
 CREATE INDEX IF NOT EXISTS idx_drp_entity ON data_retention_policies (entity_table, field_name);
 
